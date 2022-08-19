5cb221a66ae6392b9713d3fff925c44255c6f489
jtphillips215 <jtphillips215@gmail.com>
2022-08-18 21:27:56 -0500

    added type hinting on functions dealing with ID, added response codes to functions returning specific codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 # creating app as instance of FastAPI
 app = FastAPI()
@@ -12,13 +12,15 @@ def root():
 
 # PUT request to add coin
 @app.put("/")
-def add_coin():
+def add_coin(response: Response):
+    response.status_code = 204
     pass
 
 
 # DELETE request to return coins
 @app.delete("/")
-def return_coins():
+def return_coins(response: Response):
+    response.status_code = 204
     pass
 
 
@@ -30,23 +32,25 @@ def get_machine_inventory():
 
 # GET request for individual item inventory
 @app.get("/inventory/{id}")
-def get_item_inventory():
+def get_item_inventory(id: int):
     pass
 
 
 # PUT request for vending item
 @app.put("/inventory/{id}")
-def vend_item():
+def vend_item(id: int):
     pass
 
 
-# PUT request for resource/item not found 404
+# PUT request for resource/item not found or out of stock 404
 @app.put("/inventory/{id}")
-def resource_not_found():
+def resource_not_found(id: int, response: Response):
+    response.status_code = 404
     pass
 
 
 # PUT request for currency below purchase price 403
 @app.put("/inventory/")
-def currency_below_purchase_price():
+def currency_below_purchase_price(response: Response):
+    response.status_code = 403
     pass
