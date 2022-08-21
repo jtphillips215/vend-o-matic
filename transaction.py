59769c35b232be01dc67e9e6ed803ac9d2bdc91e
jtphillips215 <jtphillips215@gmail.com>
2022-08-21 18:08:36 -0500

    minor changes to transaction class

--- transaction.py
+++ transaction.py
@@ -1,18 +1,18 @@
 class Transaction:
     def __init__(self):
-        self.coin_count = 0
+        self._coin_count = 0
 
     def get_coin_count(self):
-        return self.coin_count
+        return self._coin_count
 
     def set_coin_count(self, value: int):
-        self.coin_count = value
+        self._coin_count = value
 
     def test_coin_count(self):
-        if self.coin_count >= 2:
+        if self._coin_count >= 2:
             return True
         else:
             return False
 
     def clear_coin_count(self):
-        self.coin_count = 0
+        self._coin_count = 0
